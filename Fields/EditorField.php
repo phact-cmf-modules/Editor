1c61876b370ddce02936f0c60e144c81d1cc19e2
hashstudio <hashstudio@mail.ru>
2017-02-12 17:07:42 +0300

    Multiple file editors fix

--- Fields/EditorField.php
+++ Fields/EditorField.php
@@ -19,4 +19,26 @@ use Phact\Form\Fields\TextAreaField;
 class EditorField extends TextAreaField
 {
     public $inputTemplate = 'editor/fields/editor_field_input.tpl';
+    
+    public function renderInput()
+    {
+        return $this->renderTemplate($this->inputTemplate, [
+            'rid' => self::generateRandomString(),
+            'field' => $this,
+            'html' => $this->buildAttributesInput(),
+            'id' => $this->getHtmlId(),
+            'value' => $this->getRenderValue(),
+            'name' => $this->getHtmlName()
+        ]);
+    }
+
+    protected static function generateRandomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
 }
