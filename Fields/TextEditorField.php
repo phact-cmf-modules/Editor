3ef81cb1a8b8f56a28cbae515bbbad34df10dadb
hashstudio <hashstudio@mail.ru>
2017-04-25 16:21:29 +0300

    Text

---
+++ Fields/TextEditorField.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *
+ *
+ * All rights reserved.
+ *
+ * @author Okulov Anton
+ * @email qantus@mail.ru
+ * @version 1.0
+ * @company HashStudio
+ * @site http://hashstudio.ru
+ * @date 14/11/16 12:52
+ */
+
+namespace Modules\Editor\Fields;
+
+use Phact\Orm\Fields\TextField;
+
+class TextEditorField extends TextField
+{
+    public function getFormField()
+    {
+        return $this->setUpFormField([
+            'class' => EditorField::class
+        ]);
+    }
+}
